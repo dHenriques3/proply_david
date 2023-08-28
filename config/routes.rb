1d75732c9c155d83f890a92a617f5d29af17e7b3
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-28 22:32:22 +0100

    basic controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # These are the routes for the most basic functionality.
+  # They correspond to the 4 primary tables;
+  # (properties, documents, tasks, and subtasks)
+  resources :properties do
+    resources :documents, only: %i[create]
+    resources :tasks, only: %i[create show]
+
+    # resources :tenancies, only: %i[index] # this route is extra funtionality
+    # resources :chatroom, only: %i[show] # this route is extra funtionality
+    # Does resources :messages need to be nested.
+  end
+
+  resources :documents, only: %i[update destroy]
+  resources :tasks, only: %i[update destroy] do
+    resources :subtasks, only: :create
+  end
+  resources :subtasks, only: %i[update destroy]
 end
