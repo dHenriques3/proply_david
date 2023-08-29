4c53794066ebb9db132526eaaa70f4c3409dd48a
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-29 23:42:29 +0100

    the app has been tested and is fully working

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,9 +3,9 @@ class Property < ApplicationRecord
 
   has_one :chatroom
 
-  has_many :tenancies
+  has_many :tenancies, dependent: :destroy
 
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates_presence_of :name, :property_type, :address, :description
 end
