0802c9da684901708f6467a31ae6c0a9a7f9be22
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-30 00:45:28 +0100

    document controller completed

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -10,6 +10,23 @@ class DocumentsController < ApplicationController
     end
   end
 
+  def update
+    @document = Document.find(params[:id])
+    @property = @document.property
+    if @document.update(document_params)
+      redirect_to property_path(@property), notice: "Your document was updated successfully."
+    else
+      redirect_to property_path(@property), notice: "Your document failed to update."
+    end
+  end
+
+  def delete
+    @document = Document.find(params[:id])
+    @property = @document.property
+    @document.destroy
+    redirect_to property_path(@property), notice: "Document successfully removed!"
+  end
+
   private
 
   def document_params
