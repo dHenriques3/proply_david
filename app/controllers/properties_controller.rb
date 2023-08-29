6c067e87184ebd4f9819bba040f77f306e8e05f0
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-29 14:51:02 +0100

    Add a property functionality complete

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,11 +6,16 @@ class PropertiesController < ApplicationController
   def create
     @property = Property.new(property_params)
     @property.user = current_user
+    if @property.save
+      redirect_to properties_path
+    else
+      render :new, status: :unprocessable_entity, message: "Something went wrong!"
+    end
   end
 
   private
 
   def property_params
-    params.require(:property).permit(:name, :type, :address, :description)
+    params.require(:property).permit(:name, :property_type, :address, :description)
   end
 end
