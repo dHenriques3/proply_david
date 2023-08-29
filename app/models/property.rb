db634348b06a4045598a1a71c944a373bca9fa39
Nii <eagleaddo@yahoo.co.uk>
2023-08-29 11:16:41 +0100

    Revert "Extra models"

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,3 @@
 class Property < ApplicationRecord
   belongs_to :user
-
-  has_one :chatroom
-
-  has_many :tenancies
-  has_many :documents
-
-  has_many :tasks
-  has_many :subtasks, through: :tasks
 end
