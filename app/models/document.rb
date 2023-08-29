614dec516b9e7a9b5979434f5a1c544e0e152167
Ceaser Mulenga <caesarlord11@gmail.com>
2023-08-29 15:15:56 +0100

    Validations Complete

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,5 @@
 class Document < ApplicationRecord
   belongs_to :property
+
+  validates_presence_of :title, :type
 end
--- app/models/subtask.rb
+++ app/models/subtask.rb
@@ -1,3 +1,5 @@
 class Subtask < ApplicationRecord
   belongs_to :task
+
+  validates_presence_of :title, :start_date, :completion_date
 end
