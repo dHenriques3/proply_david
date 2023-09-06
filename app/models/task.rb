613253c07e404809b1741b40531ed8408cff2026
Ceaser Mulenga <caesarlord11@gmail.com>
2023-09-06 10:11:21 +0100

    subtask edited

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,7 @@ class Task < ApplicationRecord
   belongs_to :property
   belongs_to :user
 
-  has_many :subtasks
+  has_many :subtasks, dependent: :destroy
 
   validates_presence_of :start_date, :completion_date, :title
 end
